8b481e761e86faffe76b6260a855f7de92789626
SySafarila <sysafarila.official@gmail.com>
2020-07-17 20:50:31 +0700

    Update UI

--- resources/views/components/complaint-status.blade.php
+++ resources/views/components/complaint-status.blade.php
@@ -1,5 +1,5 @@
 <div class="d-flex mb-3">
-    <a href="{{ route('complaints.index') }}" class="mr-2"><small class="badge badge-light shadow-sm">Pending | {{ $pending }}</small></a>
-    <a href="{{ route('complaints.onProcess') }}" class="mr-2"><small class="badge badge-light shadow-sm">On Process | {{ $onProcess }}</small></a>
-    <a href="{{ route('complaints.complete') }}" class="mr-2"><small class="badge badge-light shadow-sm">Complete | {{ $complete }}</small></a>
+    <a href="{{ route('complaints.index') }}" class="mr-2"><small class="badge badge-danger shadow-sm">Pending | {{ $pending }}</small></a>
+    <a href="{{ route('complaints.onProcess') }}" class="mr-2"><small class="badge badge-warning shadow-sm">On Process | {{ $onProcess }}</small></a>
+    <a href="{{ route('complaints.complete') }}" class="mr-2"><small class="badge badge-success shadow-sm">Complete | {{ $complete }}</small></a>
 </div>
